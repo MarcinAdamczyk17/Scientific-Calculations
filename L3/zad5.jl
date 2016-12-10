d0623fc8087030681023684da1a9adba821aa595
MarcinAdamczyk17 <marcin8551@gmail.com>
2016-12-10 21:58:49 +0100

    lista 3

---
+++ L3/zad5.jl
@@ -0,0 +1,11 @@
+#=
+  autor: Marcin Adamczyk
+  nr indeksu: 221429
+=#
+
+workspace();
+
+using MyModule
+
+println("metoda bisekcji: [0.0, 1.0] x = ",mbisekcji(x->(e^x)-(3*x), 0.0, 1.0, 1.0e-4,1.0e-4)[1])
+println("metoda bisekcji: [1.0, 2.0] x = ",mbisekcji(x->(e^x)-(3*x), 1.0, 2.0, 1.0e-4,1.0e-4)[1])
